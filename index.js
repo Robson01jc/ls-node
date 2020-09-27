b15bd883994c06c4fd0fe637074651b27405c899
Robson01jc <robson01jc@gmail.com>
2020-09-27 11:22:41 -0300

    Using the process.cwd() function to get the directory I've ran the node command.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const fs = require("fs");
 
+// The process module is automatically added into the global scope
+fs.readdir(process.cwd(), (err, filenames) => {
+	if (err) {
+		console.log(err);
+	}
 
+	console.log(filenames);
+});
