6252f9f33bc15ef42d2741f21d73affcb1584f36
Robson01jc <robson01jc@gmail.com>
2020-10-04 11:38:51 -0300

    Making use of the lstat method from the fs module.

--- index.js
+++ index.js
@@ -2,11 +2,38 @@
 
 const fs = require("fs");
 
-// The process module is automatically added into the global scope
-fs.readdir(process.cwd(), (err, filenames) => {
+// Method #2
+// const util = require("util");
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
 	if (err) {
 		console.log(err);
 	}
 
-	console.log(filenames);
+	try {
+		const allStats = await Promise.all(filenames.map(lstat));
+
+		allStats.forEach((stats, index) => {
+			console.log(filenames[index], stats.isFile());
+		});
+	} catch (err) {
+		console.log(err);
+	}
 });
+
+// Method #1
+// const lstat = (filename) => {
+// 	return new Promise((resolve, reject) => {
+// 		fs.lstat(filename, (err, stats) => {
+// 			if (err) {
+// 				reject(err);
+// 			}
+
+// 			resolve(stats);
+// 		});
+// 	});
+// };
